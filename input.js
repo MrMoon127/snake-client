9209c3d8fdc63fec37ed1e2ac06f1f773cb1583f
Sasha Andia <sasha.andia@gmail.com>
2023-07-01 11:49:38 -0700

    now can input WASD to send a message from the connection to move respectively

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+let connection;
+
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,12 +14,28 @@ const setupInput = function () {
 };
 
 const handleUserInput = function(key) {
-
+  // enables us to have a way to exit at any time
   if (key === '\u0003') {
 
     process.exit();
 
   };
+
+  // movement options below
+
+  if (key === 'w') {
+    connection.write("Move: up");
+  };
+  if (key === 'a') {
+    connection.write("Move: left");
+  };
+  if (key === 's') {
+    connection.write("Move: down");
+  };
+  if (key === 'd') {
+    connection.write("Move: right");
+  };
+
 };
 
 module.exports = {
